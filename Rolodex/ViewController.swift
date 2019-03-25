62020bca1c67938190af293a583bdd8a95de18cf
Christina Wolff <cwolff@cable.highpoint.edu>
2019-03-25 12:56:06 -0400

    Initial Commit

---
+++ Rolodex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rolodex
+//
+//  Created by Christina Wolff on 3/25/19.
+//  Copyright © 2019 Christina Wolff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
